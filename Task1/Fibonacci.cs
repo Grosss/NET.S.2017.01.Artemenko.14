2992323b0eec940f717ca7255e5364c795cd5e1e
Grosss <artemenko,nickolai@gmail.com>
2017-04-26 14:50:03 +0200

    task1 is added

---
+++ Task1/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class Fibonacci
+    {
+        public static IEnumerable<int> GetSequence(int termsNumber)
+        {
+            if (termsNumber < 1)
+                throw new ArgumentOutOfRangeException();
+
+            int previousTerm = 0;
+            int currentTerm = 1;
+            yield return previousTerm;
+
+            for(int i = 0; i < termsNumber - 1; i++)
+            {
+                yield return currentTerm;
+                currentTerm = previousTerm + currentTerm;
+                previousTerm = currentTerm - previousTerm;
+            }
+        }
+    }
+}
